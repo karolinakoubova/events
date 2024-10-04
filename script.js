1f8deef26c19344cd32e953a2ee06f3a72f6da8d
Karolina <karolinastella@seznam.cz>
2024-10-04 10:26:47 +0200

    fetched data from api

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const eventName = document.querySelector(".event-name")
+// fetching data from the api
+const loadData = async () => {
+    const response = await fetch('https://test-api.codingbootcamp.cz/api/147c6eae/events');
+    const data = await response.json();
+console.log(data)
+ eventName.innerHTML = data[1].name
+ 
+}
+
+loadData()
+
+
+
+
+
